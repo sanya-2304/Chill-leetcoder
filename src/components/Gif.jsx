8ac3be72daf1961292b969fe3b67a5380beb7ec0
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-12-04 12:31:48 +0530

    gif

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Gif = () => {
   return (
     <div className='flex justify-center items-center h-full w-full'>
-      <img src="https://media1.tenor.com/m/PR_MUAZt4g4AAAAd/friends-friendstv.gif" alt="Loading GIF" className='w-full h-full object-cover'/>
+      <img src="https://media.tenor.com/jVqnbrygNjMAAAAM/totoro-waiting.gif" alt="Loading GIF" className='w-full h-full object-cover'/>
     </div>
   )
 }
